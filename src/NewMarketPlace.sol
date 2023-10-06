8335daea3e45c7e6b818e72e31f50372cefca070
Damkols <damolausmankolapo@gmal.com>
2023-10-06 16:45:55 +0100

    update

---
+++ src/NewMarketPlace.sol
@@ -0,0 +1,155 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "openzeppelin/token/ERC721/IERC721.sol";
+import "openzeppelin/access/Ownable.sol";
+import "openzeppelin/utils/math/SafeMath.sol";
+
+
+contract Marketplace is Ownable {
+    using SafeMath for uint256;
+
+    // Struct to represent an NFT order
+    struct Order {
+        address token;
+        uint256 tokenId;
+        uint256 price;
+        uint88 deadline;
+        address owner;
+        bool active;
+    }
+
+    // Mapping to store orders by order ID
+    mapping(uint256 => Order) public orders;
+    uint256 public orderId = 0;
+
+    // Events to track order actions
+    event OrderCreated(uint256 indexed orderId, Order order);
+    event OrderExecuted(uint256 indexed orderId, Order order);
+    event OrderEdited(uint256 indexed orderId, Order order);
+
+    // Modifiers
+    modifier onlyNFTOwner(address token, uint256 tokenId) {
+        require(IERC721(token).ownerOf(tokenId) == msg.sender, "Not the NFT owner");
+        _;
+    }
+
+    modifier onlyActiveOrder(uint256 _orderId) {
+        require(orders[_orderId].active, "Order is not active");
+        _;
+    }
+
+    modifier orderExists(uint256 _orderId) {
+        require(_orderId < orderId, "Order does not exist");
+        _;
+    }
+
+    modifier validPrice(uint256 price) {
+        require(price >= 0.01 ether, "Minimum price not met");
+        _;
+    }
+
+    modifier validDuration(uint88 deadline) {
+        require(deadline >= block.timestamp.add(60 minutes), "Minimum duration not met");
+        _;
+    }
+
+    modifier validSignature(
+        address token,
+        uint256 tokenId,
+        uint256 price,
+        uint88 deadline,
+        address owner,
+        bytes memory signature
+    ) {
+        require(
+            isValidSignature(token, tokenId, price, deadline, owner, signature),
+            "Invalid signature"
+        );
+        _;
+    }
+
+    // Create a new NFT order
+    function createOrder(
+        address token,
+        uint256 tokenId,
+        uint256 price,
+        uint88 deadline,
+        bytes memory signature
+    ) public
+        onlyNFTOwner(token, tokenId)
+        onlyActiveOrder(orderId)
+        validPrice(price)
+        validDuration(deadline)
+        validSignature(token, tokenId, price, deadline, msg.sender, signature)
+    {
+        orders[orderId] = Order({
+            token: token,
+            tokenId: tokenId,
+            price: price,
+            deadline: deadline,
+            owner: msg.sender,
+            active: true
+        });
+
+        emit OrderCreated(orderId, orders[orderId]);
+        orderId++;
+    }
+
+    // Execute an NFT order
+    function executeOrder(uint256 _orderId) public payable
+        orderExists(_orderId)
+        onlyActiveOrder(_orderId)
+    {
+        Order storage order = orders[_orderId];
+        require(order.deadline >= block.timestamp, "Order has expired");
+        require(msg.value >= order.price, "Insufficient funds");
+
+        order.active = false;
+
+        IERC721(order.token).transferFrom(order.owner, msg.sender, order.tokenId);
+        payable(order.owner).transfer(order.price);
+
+        emit OrderExecuted(_orderId, order);
+    }
+
+    // Edit an existing NFT order
+   // Edit an existing NFT order
+    function editOrder(
+        uint256 _orderId,
+        uint256 _newPrice,
+        bool _active
+    ) public
+        onlyNFTOwner(orders[_orderId].token, orders[_orderId].tokenId)
+        orderExists(_orderId)
+    {
+        Order storage order = orders[_orderId];
+        order.price = _newPrice;
+        order.active = _active;
+
+        emit OrderEdited(_orderId, order);
+    }
+
+
+    // Check if a signature is valid
+    function isValidSignature(
+        address token,
+        uint256 tokenId,
+        uint256 price,
+        uint88 deadline,
+        address owner,
+        bytes memory signature
+    ) internal view returns (bool) {
+        // Implement your signature verification logic here
+        // Example: return true if signature is valid, otherwise false
+        return true;
+    }
+
+    // Get details of an NFT order
+    function getOrder(uint256 _orderId) public view
+        orderExists(_orderId)
+        returns (Order memory)
+    {
+        return orders[_orderId];
+    }
+}
--- test/Market.t.sol
+++ test/Market.t.sol
@@ -6,243 +6,129 @@ import "../src/ERC721Mock.sol";
 import "./Helpers.sol";
 
 contract MarketPlaceTest is Helpers {
-    Marketplace mPlace;
-    OurNFT nft;
+  Marketplace mPlace;
+  OurNFT nft;
 
-    uint256 currentOrderId;
+  uint256 currentOrderId;
 
-    address userA;
-    address userB;
+  address userA;
+  address userB;
 
-    uint256 privKeyA;
-    uint256 privKeyB;
+  uint256 privKeyA;
+  uint256 privKeyB;
 
-    Marketplace.Order order;
+  Marketplace.Order order;
 
-    function setUp() public {
-        mPlace = new Marketplace();
-        nft = new OurNFT();
+  function setUp() public {
+    mPlace = new Marketplace();
+    nft = new OurNFT();
 
-        (userA, privKeyA) = mkaddr("USERA");
-        (userB, privKeyB) = mkaddr("USERB");
+    (userA, privKeyA) = mkaddr("USERA");
+    (userB, privKeyB) = mkaddr("USERB");
 
-        order = Marketplace.Order({
-            token: address(nft),
-            tokenId: 1,
-            price: 1 ether,
-            signature: bytes(""),
-            deadline: 0,
-            owner: address(0),
-            active: false
-        });
+    order = Marketplace.Order({
+      token: address(nft),
+      tokenId: 1,
+      price: 1 ether,
+      signature: bytes(""),
+      deadline: 0,
+      owner: address(0),
+      active: false
+    });
 
-        nft.mint(userA, 1);
-    }
+    nft.mint(userA, 1);
+  }
 
-    function testOwnerCannotCreateOrder() public {
-        order.owner = userB;
-        switchSigner(userB);
+  /// Tests that a non-owner cannot create an order.
+  function testOwnerCannotCreateOrder() public {
+    order.owner = userB;
+    switchSigner(userB);
 
-        vm.expectRevert(Marketplace.NotOwner.selector);
-        mPlace.createOrder(order);
-    }
+    vm.expectRevert(Marketplace.NotOwner.selector);
+    mPlace.createOrder(order);
+  }
 
-    function testNFTNotApproved() public {
-        switchSigner(userA);
-        vm.expectRevert(Marketplace.NotApproved.selector);
-        mPlace.createOrder(order);
-    }
+  /// Tests that an NFT must be approved before an order can be created.
+  function testNFTNotApproved() public {
+    switchSigner(userA);
 
-    function testMinPriceTooLow() public {
-        switchSigner(userA);
-        nft.setApprovalForAll(address(mPlace), true);
-        order.price = 0;
-        vm.expectRevert(Marketplace.MinPriceTooLow.selector);
-        mPlace.createOrder(order);
-    }
+    vm.expectRevert(Marketplace.NotApproved.selector);
+    mPlace.createOrder(order);
+  }
 
-    function testMinDeadline() public {
-        switchSigner(userA);
-        nft.setApprovalForAll(address(mPlace), true);
-        vm.expectRevert(Marketplace.DeadlineTooSoon.selector);
-        mPlace.createOrder(order);
-    }
+  /// Tests that the minimum price cannot be met.
+  function testMinPriceTooLow() public {
+    switchSigner(userA);
+    nft.setApprovalForAll(address(mPlace), true);
+    order.price = 0;
 
-    function testMinDuration() public {
-        switchSigner(userA);
-        nft.setApprovalForAll(address(mPlace), true);
-        order.deadline = uint88(block.timestamp + 59 minutes);
-        vm.expectRevert(Marketplace.MinDurationNotMet.selector);
-        mPlace.createOrder(order);
-    }
+    vm.expectRevert(Marketplace.MinPriceTooLow.selector);
+    mPlace.createOrder(order);
+  }
 
-    function testSignatureNotValid() public {
-        // Test that signature is valid
-        switchSigner(userA);
-        nft.setApprovalForAll(address(mPlace), true);
-        order.deadline = uint88(block.timestamp + 120 minutes);
-        order.signature = constructSig(
-            order.token,
-            order.tokenId,
-            order.price,
-            order.deadline,
-            order.owner,
-            privKeyB
-        );
-        vm.expectRevert(Marketplace.InvalidSignature.selector);
-        mPlace.createOrder(order);
-    }
+  /// Tests that the minimum deadline cannot be met.
+  function testMinDeadline() public {
+    switchSigner(userA);
+    nft.setApprovalForAll(address(mPlace), true);
 
-    // EDIT Order
-    function testEditNonValidOrder() public {
-        switchSigner(userA);
-        vm.expectRevert(Marketplace.OrderNotExistent.selector);
-        mPlace.editOrder(1, 0, false);
-    }
+    vm.expectRevert(Marketplace.DeadlineTooSoon.selector);
+    mPlace.createOrder(order);
+  }
 
-    function testEditOrderNotOwner() public {
-        switchSigner(userA);
-        nft.setApprovalForAll(address(mPlace), true);
-        order.deadline = uint88(block.timestamp + 120 minutes);
-        order.signature = constructSig(
-            order.token,
-            order.tokenId,
-            order.price,
-            order.deadline,
-            order.owner,
-            privKeyA
-        );
-        // vm.expectRevert(Marketplace.OrderNotExistent.selector);
-        uint256 newOrderId = mPlace.createOrder(order);
+  /// Tests that the minimum duration cannot be met.
+  function testMinDuration() public {
+    switchSigner(userA);
+    nft.setApprovalForAll(address(mPlace), true);
+    order.deadline = uint88(block.timestamp + 59 minutes);
 
-        switchSigner(userB);
-        vm.expectRevert(Marketplace.NotOwner.selector);
-        mPlace.editOrder(newOrderId, 0, false);
-    }
+    vm.expectRevert(Marketplace.MinDurationNotMet.selector);
+    mPlace.createOrder(order);
+  }
 
-    function testEditOrder() public {
-        switchSigner(userA);
-        nft.setApprovalForAll(address(mPlace), true);
-        order.deadline = uint88(block.timestamp + 120 minutes);
-        order.signature = constructSig(
-            order.token,
-            order.tokenId,
-            order.price,
-            order.deadline,
-            order.owner,
-            privKeyA
-        );
-        uint256 newOrderId = mPlace.createOrder(order);
-        mPlace.editOrder(newOrderId, 0.01 ether, false);
+  /// Tests that a signature must be valid.
+  function testSignatureNotValid() public {
+    // Test that signature is invalid
+    switchSigner(userA);
+    nft.setApprovalForAll(address(mPlace), true);
+    order.deadline = uint88(block.timestamp + 120 minutes);
+    order.signature = constructSig(
+      order.token,
+      order.tokenId,
+      order.price,
+      order.deadline,
+      order.owner,
+      privKeyB
+    );
 
-        Marketplace.Order memory _order = mPlace.getOrder(newOrderId);
-        assertEq(_order.price, 0.01 ether);
-        assertEq(_order.active, false);
-    }
+    vm.expectRevert(Marketplace.InvalidSignature.selector);
+    mPlace.createOrder(order);
+  }
 
-    // EXECUTE Order
-    function testExecuteNonValidOrder() public {
-        switchSigner(userA);
-        vm.expectRevert(Marketplace.OrderNotExistent.selector);
-        mPlace.executeOrder(1);
-    }
+  /// Tests that a non-existent order cannot be edited.
+  function testEditNonValidOrder() public {
+    switchSigner(userA);
 
-    function testExecuteExpiredOrder() public {
-        switchSigner(userA);
-        nft.setApprovalForAll(address(mPlace), true);
-    }
+    vm.expectRevert(Marketplace.OrderNotExistent.selector);
+    mPlace.editOrder(1, 0, false);
+  }
 
-    function testExecuteOrderNotActive() public {
-        switchSigner(userA);
-        nft.setApprovalForAll(address(mPlace), true);
-        order.deadline = uint88(block.timestamp + 120 minutes);
-        order.signature = constructSig(
-            order.token,
-            order.tokenId,
-            order.price,
-            order.deadline,
-            order.owner,
-            privKeyA
-        );
-        uint256 newOrderId = mPlace.createOrder(order);
-        mPlace.editOrder(newOrderId, 0.01 ether, false);
-        switchSigner(userB);
-        vm.expectRevert(Marketplace.OrderNotActive.selector);
-        mPlace.executeOrder(newOrderId);
-    }
+  /// Tests that a non-owner cannot edit an order.
+  function testEditOrderNotOwner() public {
+    switchSigner(userA);
+    nft.setApprovalForAll(address(mPlace), true);
+    order.deadline = uint88(block.timestamp + 120 minutes);
+    order.signature = constructSig(
+      order.token,
+      order.tokenId,
+      order.price,
+      order.deadline,
+      order.owner,
+      privKeyA
+    );
 
-    function testFulfilOrderPriceNotEqual() public {
-        switchSigner(userA);
-        nft.setApprovalForAll(address(mPlace), true);
-        order.deadline = uint88(block.timestamp + 120 minutes);
-        order.signature = constructSig(
-            order.token,
-            order.tokenId,
-            order.price,
-            order.deadline,
-            order.owner,
-            privKeyA
-        );
-        uint256 newOrderId = mPlace.createOrder(order);
-        switchSigner(userB);
-        vm.expectRevert(
-            abi.encodeWithSelector(
-                Marketplace.PriceNotMet.selector,
-                order.price - 0.9 ether
-            )
-        );
-        mPlace.executeOrder{value: 0.9 ether}(newOrderId);
-    }
+    uint256 newOrderId = mPlace.createOrder(order);
 
-    function testFulfilOrderPriceMismatch() public {
-        switchSigner(userA);
-        nft.setApprovalForAll(address(mPlace), true);
-        order.deadline = uint88(block.timestamp + 120 minutes);
-        order.signature = constructSig(
-            order.token,
-            order.tokenId,
-            order.price,
-            order.deadline,
-            order.owner,
-            privKeyA
-        );
-        uint256 newOrderId = mPlace.createOrder(order);
-        switchSigner(userB);
-        vm.expectRevert(
-            abi.encodeWithSelector(
-                Marketplace.PriceMismatch.selector,
-                order.price
-            )
-        );
-        mPlace.executeOrder{value: 1.1 ether}(newOrderId);
-    }
+    switchSigner(userB);
 
-    function testFulfilOrder() public {
-        switchSigner(userA);
-        nft.setApprovalForAll(address(mPlace), true);
-        order.deadline = uint88(block.timestamp + 120 minutes);
-        order.signature = constructSig(
-            order.token,
-            order.tokenId,
-            order.price,
-            order.deadline,
-            order.owner,
-            privKeyA
-        );
-        uint256 newOrderId = mPlace.createOrder(order);
-        switchSigner(userB);
-        uint256 userABalanceBefore = userA.balance;
-
-        mPlace.executeOrder{value: order.price}(newOrderId);
-
-        uint256 userABalanceAfter = userA.balance;
-
-        Marketplace.Order memory _order = mPlace.getOrder(newOrderId);
-        assertEq(_order.price, 1 ether);
-        assertEq(_order.active, false);
-
-        assertEq(_order.active, false);
-        assertEq(ERC721(order.token).ownerOf(order.tokenId), userB);
-        assertEq(userABalanceAfter, userABalanceBefore + order.price);
-    }
-}
+    vm.expectRevert(Marketplace.NotOwner.selector);
+    mPlace.edit
